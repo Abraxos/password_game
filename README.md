fc900bfe8662fe36e16e3c73663d6923d1e9f845
Eugene Kovalev <ekovalev@akamai.com>
2017-04-04 08:17:51 -0400

    Initial commit, contains basic game code and configuration.

--- README.md
+++ README.md
@@ -1,2 +1,74 @@
-# password_game
+# Password Game
+
 A hacking game for little kids allowing then to try to "hack" into a login page given some information about the users
+
+## Disclaimer
+
+This is a hacking game for little kids. The word "insecure" does not even begin to describe this webapp. If you use any component of this website as an example of security in any way, shape, or form, you have only yourself to blame.
+
+## Setup
+
+It is recommended that this game be set up in a virtual environment like so:
+
+```
+$ git clone git@github.com:Abraxos/password_game.git
+$ cd password_game
+$ mkdir .password_game_venv
+$ virtualenv -p /usr/local/bin/python3.5 .password_game_venv # or wherever you have the python 3.5 binary
+```
+
+Activate the environment and install pre-requisite packages:
+
+```
+$ . .password_game_venv/bin/activate
+(.password_game_venv) $ pip install flask flask_wtf
+```
+
+## Usage
+
+Once you have the environment set up per instructions above, you can launch the webapp through the following command:
+
+```
+(.password_game_venv) $ python password_game.py
+```
+
+### Configuration
+
+The game expects a configuration file called `config.ini` to be in the current working directory. This file will have a bunch of entries. The entry labeled `[Password Game]` is used to configure the webapp itself:
+
+```
+[Password Game]
+port = 5000
+password_file = passwd
+username_file = username
+attempts_file = attempts
+log_file = main.log
+```
+
+The password file is where the current password is stored. The username file is where the current username is stored, and the attempts file logs failed attempts to guess the password right up until someone guesses correctly at which point the attempts file is erased.
+
+The rest of the configuration has sections that look something like this:
+
+```
+[SleevelessChampion]
+name = Albert T. Nichols
+password = football
+mother's maiden name = Diaz
+date of birth = March 27, 1968
+hint = Uses their favorite sport as their password
+hobbies = Sports
+name of pet = Teddy
+favorite color = Red
+favorite sports team = Patriots
+```
+
+The name of the section is the person's username and the rest are information about the person in question. It is recommended that all properties be filled out. The usernames must be unique in order for the game to function properly.
+
+### Pages
+
+There are 4 pages that are part of this webapp:
+
++ `/login/` - Displays the actual login prompt as well as the previous attempts to log in.
++ `/view/` - Displays a list of users with links to their personal pages.
++ `/view/<username>/` - Displays the personal page of a given user which shows all their details except their password.
++ `/admin` - This page should be kept secret from the players. It shows a list of users along with their usernames and passwords. Clicking on a password sets that user's username to the username file and password to the password file. This effectively makes that person the target of the hacking game because in order to win, the players must figure out that user's password.
---
+++ password_game.py
@@ -0,0 +1,155 @@
+'''A webapp that makes game for kids to guess passwords. The admin interface
+   allows you to choose a character whose password needs to be guessed'''
+from logging.handlers import RotatingFileHandler
+from logging import INFO
+from configparser import ConfigParser
+from collections import namedtuple
+
+from flask import Flask
+from flask import render_template, redirect, url_for, request
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, PasswordField
+
+# Constants
+APP_NAME = 'Password Game'
+PASSWD_FILE_KEY = 'password_file'
+USERNM_FILE_KEY = 'username_file'
+ATTEMPTS_FILE_KEY = 'attempts_file'
+LOG_FILE_KEY = 'log_file'
+PORT_KEY = 'port'
+CONFIG_PATH = 'config.ini'
+
+APP = Flask(APP_NAME)
+APP.secret_key = 'debug key'
+CONFIG = ConfigParser()
+
+User = namedtuple('User', ['username', 'name', 'mother_maiden_name',
+                           'date_of_birth', 'hint', 'hobbies', 'password',
+                           'name_of_pet', 'favorite_color',
+                           'favorite_sports_team'])
+Attempt = namedtuple('Attempt', ['username', 'password'])
+
+def get_attempts():
+    '''Returns the contents of the attempts file as a list of objects.'''
+    attempts = []
+    with open(CONFIG[APP_NAME][ATTEMPTS_FILE_KEY], 'r') as attempts_file:
+        for line in attempts_file.readlines():
+            username = line.split(' : ', 1)[0]
+            password = line.split(' : ', 1)[1]
+            attempts.append(Attempt(username=username, password=password))
+    if attempts:
+        return reversed(attempts)
+    else:
+        return None
+
+def get_prop(user, prop):
+    '''Accessor for a property of a particular user.'''
+    if user in CONFIG:
+        if prop in CONFIG[user]:
+            return CONFIG[user][prop]
+    else:
+        return ''
+
+def get_user(username):
+    '''Retrieves information about a user and packs it as a namedtuple.'''
+    if username in CONFIG:
+        user = User(username=username,
+                    name=get_prop(username, 'name'),
+                    password=get_prop(username, 'password'),
+                    mother_maiden_name=get_prop(username, "mother's maiden name"),
+                    date_of_birth=get_prop(username, 'date of birth'),
+                    hint=get_prop(username, 'hint'),
+                    hobbies=get_prop(username, 'hobbies'),
+                    name_of_pet=get_prop(username, 'name of pet'),
+                    favorite_color=get_prop(username, 'favorite color'),
+                    favorite_sports_team=get_prop(username, 'favorite sports team'))
+        return user
+
+@APP.route('/view/')
+def view():
+    '''Displays a list of available users with links to select them.'''
+    usernames = [u for u in CONFIG if u not in [APP_NAME, 'DEFAULT']]
+    return render_template('view.html', users=[get_user(u) for u in usernames])
+
+@APP.route('/view/<string:username>')
+def view_username(username):
+    '''Displays the details of a single user.'''
+    if username in CONFIG:
+        return render_template('view_username.html', user=get_user(username))
+    else:
+        return "No such user!"
+
+
+class LoginForm(FlaskForm):
+    '''A simple login form for the user to guess passwords'''
+    username = StringField('username')
+    password = PasswordField('password')
+    login_button = SubmitField('Login')
+
+@APP.route('/login/')
+def login():
+    '''Presents the user with a login page.'''
+    return render_template('login.html', form=LoginForm(),
+                           attempts=get_attempts())
+
+def username_passwd_correct(candidate_username, candidate_password):
+    '''Checks whether the username and password are correct.'''
+    with open(CONFIG[APP_NAME][PASSWD_FILE_KEY], 'r') as passwd_file:
+        actual_password = passwd_file.read()
+    with open(CONFIG[APP_NAME][USERNM_FILE_KEY], 'r') as usernm_file:
+        actual_username = usernm_file.read()
+    APP.logger.debug("Checking:\n\tUsername: {} ? {}\n\tPassword: {} ? {}"
+                     .format(candidate_username, actual_username,
+                             candidate_password, actual_password))
+    return candidate_username == actual_username and \
+           candidate_password == actual_password
+
+@APP.route('/login_result/', methods=['GET', 'POST'])
+def login_result():
+    '''Informs the user that they have successfully logged in.'''
+    APP.logger.debug('Computing login result')
+    candidate_username = request.form['username']
+    candidate_password = request.form['password']
+    if username_passwd_correct(candidate_username, candidate_password):
+        with open(CONFIG[APP_NAME][ATTEMPTS_FILE_KEY], 'w+') as attempts_file:
+            attempts_file.write('')
+        return render_template('login_successful.html')
+    else:
+        with open(CONFIG[APP_NAME][ATTEMPTS_FILE_KEY], 'a+') as attempts_file:
+            attempts_file.write("{} : {}\n".format(candidate_username,
+                                                 candidate_password))
+
+        return render_template('login_failure.html', form=LoginForm(),
+                               attempts=get_attempts())
+
+@APP.route('/admin/')
+def admin():
+    '''Displays a list of available users with links to set their passwords as
+       the current password.'''
+    usernames = [u for u in CONFIG if u not in [APP_NAME, 'DEFAULT']]
+    return render_template('admin.html', users=[get_user(u) for u in usernames])
+
+@APP.route('/set_user/<string:username>')
+def set_user(username):
+    '''Sets the user and password for the current round of the game'''
+    with open(CONFIG[APP_NAME][PASSWD_FILE_KEY], 'w+') as passwd_file:
+        passwd_file.write(CONFIG[username]['password'])
+    with open(CONFIG[APP_NAME][USERNM_FILE_KEY], 'w+') as usernm_file:
+        usernm_file.write(username)
+    return redirect(url_for('admin'))
+
+def parse_configuration(config_path):
+    '''Parses a given configuration file and updates the global CONFIG.'''
+    CONFIG.read(config_path)
+
+def main():
+    '''Main function, loads config, initializes logging, starts the app.'''
+    parse_configuration(CONFIG_PATH)
+    handler = RotatingFileHandler(CONFIG[APP_NAME][LOG_FILE_KEY],
+                                  maxBytes=10000, backupCount=1)
+    handler.setLevel(INFO)
+    APP.logger.addHandler(handler)
+    APP.run(port=int(CONFIG[APP_NAME][PORT_KEY]), debug=True)
+
+if __name__ == '__main__':
+    main()
